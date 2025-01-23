2770b1c49c481870e7997af4cbe065f720541217
Andrew Deacon <andrew.deacon@equalexperts.com>
2025-01-23 15:24:56 +0000

    Extend test

--- test/specs/home.e2e.js
+++ test/specs/home.e2e.js
@@ -1,7 +1,12 @@
-import { browser } from '@wdio/globals'
+import { $, browser } from '@wdio/globals'
 
 describe('Home page', () => {
-  it('Should be on the "Home" page', async () => {
-    await browser.url('/preview/draft/e2e-form/whats-your-name')
+  it('Should be progress the journey', async () => {
+    await browser.url('/grants/start')
+
+    await expect(browser).toHaveTitle(`Start page - Grants - GOV.UK`)
+    await $(`//button[@type='submit']`).click()
+
+    await expect(browser).toHaveTitle(`Are you in England? - Grants - GOV.UK`)
   })
 })
