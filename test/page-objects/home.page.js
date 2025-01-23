f50f441c91ebdd56e55212b90f6a75806e674656
Andrew Deacon <andrew.deacon@equalexperts.com>
2025-01-23 12:35:40 +0000

    Use previous URL to establish successful test

--- test/page-objects/home.page.js
+++ test/page-objects/home.page.js
@@ -2,7 +2,7 @@ import { Page } from 'page-objects/page'
 
 class HomePage extends Page {
   open() {
-    return super.open('/grants/start')
+    return super.open('/preview/draft/e2e-form/whats-your-name')
   }
 }
 
